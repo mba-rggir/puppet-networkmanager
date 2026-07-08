15154b93af9facdd617eb6d8a8924a67c1e302b6
rsguera <ruggierosguera@gmail.com>
2026-07-08 09:27:24 +0200

    Skip IPv4/IPv6 settings for OVS slave ports to prevent nmcli crashes

--- lib/puppet/provider/networkmanager_connection/networkmanager_connection.rb
+++ lib/puppet/provider/networkmanager_connection/networkmanager_connection.rb
@@ -153,6 +153,14 @@ class Puppet::Provider::NetworkmanagerConnection::NetworkmanagerConnection < Pup
 
     PROPERTY_MAP.each do |key, nmcli_key|
       next unless resource.key?(key)
+      
+      # Check if this resource is configured as an OVS slave port
+      is_ovs_slave = resource[:slave_type].to_s == 'ovs-port' || resource[:master]
+
+      # Skip IPv4 and IPv6 settings if the interface is an OVS slave to prevent nmcli from crashing
+      if is_ovs_slave && (nmcli_key.start_with?('ipv4.') || nmcli_key.start_with?('ipv6.'))
+        next
+      end
 
       value = normalize_setting_value(resource[key])
       modifications += [nmcli_key, value]
